2360fb26f5f0a14b7baf47a77402d0e2a4e0a8b5
roperluo <roperluo@tencent.com>
2020-02-26 21:47:31 +0800

    feat: ci

--- storage/redis_saver.go
+++ storage/redis_saver.go
@@ -3,14 +3,21 @@ package storage
 import (
 	"ip_proxy/producer"
 	"github.com/garyburd/redigo/redis"
+	"sync"
 )
 
+var once sync.Once
+
 type RedisSaver struct {
-	
+	c redis.Conn
 }
 
-func (rs *RedisSaver) SaveIpItems(items []producer.IPItem) error {
+func (rs *RedisSaver) openRedisConn() error {
 
+}
+func (rs *RedisSaver) SaveIpItems(items []producer.IPItem) error {
+	once.Do()
+	c, err := redis.Dial("tcp", "127.0.0.1:6379")
 	return nil
 }
 
