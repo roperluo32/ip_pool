2cd39acf15f70444e2d56263acb5414ccd3ac4a9
roperluo <roperluo.me@gmail.com>
2020-03-01 18:07:03 +0800

    feat: 添加拉去代理的webserver

--- Makefile
+++ Makefile
@@ -4,9 +4,10 @@ all: build
 
 build:
 	go build -o proxy_pool ./concretepool
+	go build -o webserver main.go
 test:
 	go test ./...
 
 clean:
-	rm -f ip_proxy
 	rm -f proxy_pool
+	rm -f webserver
--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"ip_proxy/abstract/storage"
+	"ip_proxy/component/config"
 	"ip_proxy/component/log"
+	"ip_proxy/component/storage/redisstorage"
 	"net/http"
 )
+
+var _storage storage.ProxyStorage
+
+func init() {
+	config.Init("conf", ".")
+	_storage = redisstorage.NewReidsSaver()
+}
 
 func main() {
 	r := gin.Default()
@@ -13,6 +24,21 @@ func main() {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+
+	r.GET("/get", func(c *gin.Context) {
+		domain := c.Query("domain")
+		if domain == "" {
+			c.String(200, "fail.not effect domain")
+		}
+		proxy, err := _storage.GetOneValidProxy(domain)
+		if err != nil {
+			c.String(200, "fail")
+			return
+		}
+		log.Infof("get proxy req.domain:%v, proxy:%v", domain, proxy)
+		c.String(200, fmt.Sprintf("%v:%v", proxy.IP, proxy.Port))
+		return
 	})
 
 	http.ListenAndServe(":8888", r)
